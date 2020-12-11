55e5de3c6028acacbbf08aa56f5c63f8ee8b3da9
Nina Diem <diem.nina@hotmail.com>
2020-12-11 18:15:38 +0100

    Teaser Start

--- Postillon_Newsportal/src/components/Teaser/index.jsx
+++ Postillon_Newsportal/src/components/Teaser/index.jsx
@@ -5,10 +5,11 @@ const Teaser = ({ src, genre, title, subtitle, type }) => {
 	return (
 		<div className={`teaser teaser--${type}`}>
 			<img src={src} alt="Testbild" />
-			<div className="text--box" />
-			<h4>{genre}</h4>
-			<h1>{title}</h1>
-			<p>{subtitle}</p>
+			<div className="text--box">
+				<h4>{genre}</h4>
+				<h1>{title}</h1>
+				<p>{subtitle}</p>
+			</div>
 		</div>
 	);
 };
