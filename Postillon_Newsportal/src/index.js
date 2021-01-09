f81b52af212d36729265cf7a762caaa8f9220685
Nina Diem <diem.nina@hotmail.com>
2021-01-09 09:40:53 +0100

    no message

--- Postillon_Newsportal/src/index.js
+++ Postillon_Newsportal/src/index.js
@@ -11,40 +11,40 @@ import Like, { convert as likeConvert } from "./components/Like/index.jsx";
 import Teaser from "./components/Teaser/index.jsx";
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  Debug();
-  Menu();
-  startReact("[component='like']", Like, likeConvert);
-  startReact("[component='teaser']", Teaser);
+	Debug();
+	Menu();
+	startReact("[component='like']", Like, likeConvert);
+	startReact("[component='teaser']", Teaser);
 });
 
 // get all attributes
 const getAllAttributes = (el) =>
-  el.getAttributeNames().reduce(
-    (obj, name) => ({
-      ...obj,
-      [name]: el.getAttribute(name),
-    }),
-    {}
-  );
+	el.getAttributeNames().reduce(
+		(obj, name) => ({
+			...obj,
+			[name]: el.getAttribute(name),
+		}),
+		{}
+	);
 
 const startReact = (selector, component, propsConvert) => {
-  const elements = document.querySelectorAll(selector);
+	const elements = document.querySelectorAll(selector);
 
-  elements.forEach((element) => {
-    // get props
-    const { class: _, component: __, ...props } = getAllAttributes(element);
-    // remove attributes
-    [...element.attributes].forEach((attr) =>
-      element.removeAttribute(attr.name)
-    );
-    // mount
-    ReactDOM.render(
-      React.createElement(
-        component,
-        propsConvert ? propsConvert(props) : props,
-        null
-      ),
-      element
-    );
-  });
+	elements.forEach((element) => {
+		// get props
+		const { class: _, component: __, ...props } = getAllAttributes(element);
+		// remove attributes
+		[...element.attributes].forEach((attr) =>
+			element.removeAttribute(attr.name)
+		);
+		// mount
+		ReactDOM.render(
+			React.createElement(
+				component,
+				propsConvert ? propsConvert(props) : props,
+				null
+			),
+			element
+		);
+	});
 };
